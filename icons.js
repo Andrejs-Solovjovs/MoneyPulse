0d011c7a91c8aa6f136ea66cd1164e590565ab7e
Andrejs <125615728+Andrejs-Solovjovs@users.noreply.github.com>
2026-05-12 10:06:52 +0300

    Update icons.js

--- icons.js
+++ icons.js
@@ -1,4 +1,4 @@
-const icons = {
+window.icons = {
   food: `
     <svg class="app-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
       <path d="m12 14-1 1"/>
