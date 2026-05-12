0935a6b40384904de8d8e2bee2e55fe5c3f32dfd
Andrejs <125615728+Andrejs-Solovjovs@users.noreply.github.com>
2026-05-12 11:59:16 +0300

    Icons for insights

--- icons.js
+++ icons.js
@@ -182,4 +182,294 @@ gauge: `
     <path d="M3.34 19a10 10 0 1 1 17.32 0"/>
   </svg>
 `,
+    budgetUnset: `
+    <svg class="app-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+      <path d="M17 14h.01"/>
+      <path d="M7 7h12a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h14"/>
+    </svg>
+  `,
+
+  budgetGood: `
+    <svg class="app-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+      <path d="M20 13c0 5-3.5 7.5-7.66 8.95a1 1 0 0 1-.67-.01C7.5 20.5 4 18 4 13V6a1 1 0 0 1 1-1c2 0 4.5-1.2 6.24-2.72a1.17 1.17 0 0 1 1.52 0C14.51 3.81 17 5 19 5a1 1 0 0 1 1 1z"/>
+      <path d="m9 12 2 2 4-4"/>
+    </svg>
+  `,
+
+  budgetNearLimit: `
+    <svg class="app-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+      <path d="M15.6 2.7a10 10 0 1 0 5.7 5.7"/>
+      <circle cx="12" cy="12" r="2"/>
+      <path d="M13.4 10.6 19 5"/>
+    </svg>
+  `,
+
+  budgetRisk: `
+    <svg class="app-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+      <path d="M12 12v4"/>
+      <path d="M12 20h.01"/>
+      <path d="M8.128 16.949A7 7 0 1 1 15.71 8h1.79a1 1 0 0 1 0 9h-1.642"/>
+    </svg>
+  `,
+
+  budgetOver: `
+    <svg class="app-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+      <path d="M7 18v-6a5 5 0 1 1 10 0v6"/>
+      <path d="M5 21a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-1a2 2 0 0 0-2-2H7a2 2 0 0 0-2 2z"/>
+      <path d="M21 12h1"/>
+      <path d="M18.5 4.5 18 5"/>
+      <path d="M2 12h1"/>
+      <path d="M12 2v1"/>
+      <path d="m4.929 4.929.707.707"/>
+      <path d="M12 12v6"/>
+    </svg>
+  `,
+
+  riskDayEmpty: `
+    <svg class="app-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+      <path d="M8 2v4"/>
+      <path d="M16 2v4"/>
+      <path d="M21 13V6a2 2 0 0 0-2-2H5a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h8"/>
+      <path d="M3 10h18"/>
+      <path d="m17 22 5-5"/>
+      <path d="m17 17 5 5"/>
+    </svg>
+  `,
+
+  riskDayNormal: `
+    <svg class="app-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+      <path d="M3 20a2 2 0 0 0 2 2h10a2.4 2.4 0 0 0 1.706-.706l3.588-3.588A2.4 2.4 0 0 0 21 16V6a2 2 0 0 0-2-2H5a2 2 0 0 0-2 2z"/>
+      <path d="M15 22v-5a1 1 0 0 1 1-1h5"/>
+      <path d="M8 2v4"/>
+      <path d="M16 2v4"/>
+      <path d="M3 10h18"/>
+    </svg>
+  `,
+
+  riskDayFrequent: `
+    <svg class="app-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+      <path d="M16 14v2.2l1.6 1"/>
+      <path d="M16 2v4"/>
+      <path d="M21 7.5V6a2 2 0 0 0-2-2H5a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h3.5"/>
+      <path d="M3 10h5"/>
+      <path d="M8 2v4"/>
+      <circle cx="16" cy="16" r="6"/>
+    </svg>
+  `,
+
+  riskDayExpensive: `
+    <svg class="app-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+      <rect width="18" height="18" x="3" y="4" rx="2"/>
+      <path d="M16 2v4"/>
+      <path d="M3 10h18"/>
+      <path d="M8 2v4"/>
+      <path d="M17 14h-6"/>
+      <path d="M13 18H7"/>
+      <path d="M7 14h.01"/>
+      <path d="M17 18h.01"/>
+    </svg>
+  `,
+
+  impulseZero: `
+    <svg class="app-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+      <path d="M3.85 8.62a4 4 0 0 1 4.78-4.77 4 4 0 0 1 6.74 0 4 4 0 0 1 4.78 4.78 4 4 0 0 1 0 6.74 4 4 0 0 1-4.77 4.78 4 4 0 0 1-6.75 0 4 4 0 0 1-4.78-4.77 4 4 0 0 1 0-6.76Z"/>
+      <path d="m9 12 2 2 4-4"/>
+    </svg>
+  `,
+
+  impulseLow: `
+    <svg class="app-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+      <path d="M6.3 20.3a2.4 2.4 0 0 0 3.4 0L12 18l-6-6-2.3 2.3a2.4 2.4 0 0 0 0 3.4Z"/>
+      <path d="m2 22 3-3"/>
+      <path d="M7.5 13.5 10 11"/>
+      <path d="M10.5 16.5 13 14"/>
+      <path d="m18 3-4 4h6l-4 4"/>
+    </svg>
+  `,
+
+  impulseMedium: `
+    <svg class="app-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+      <path d="M11.017 2.814a1 1 0 0 1 1.966 0l1.051 5.558a2 2 0 0 0 1.594 1.594l5.558 1.051a1 1 0 0 1 0 1.966l-5.558 1.051a2 2 0 0 0-1.594 1.594l-1.051 5.558a1 1 0 0 1-1.966 0l-1.051-5.558a2 2 0 0 0-1.594-1.594l-5.558-1.051a1 1 0 0 1 0-1.966l5.558-1.051a2 2 0 0 0 1.594-1.594z"/>
+      <path d="M20 2v4"/>
+      <path d="M22 4h-4"/>
+      <circle cx="4" cy="20" r="2"/>
+    </svg>
+  `,
+
+  impulseHigh: `
+    <svg class="app-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+      <path d="M18.518 17.347A7 7 0 0 1 14 19"/>
+      <path d="M18.8 4A11 11 0 0 1 20 9"/>
+      <path d="M9 9h.01"/>
+      <circle cx="20" cy="16" r="2"/>
+      <circle cx="9" cy="9" r="7"/>
+      <rect x="4" y="16" width="10" height="6" rx="2"/>
+    </svg>
+  `,
+
+  forecastEmpty: `
+    <svg class="app-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+      <path d="m2 2 20 20"/>
+      <path d="M8.35 2.69A10 10 0 0 1 21.3 15.65"/>
+      <path d="M19.08 19.08A10 10 0 1 1 4.92 4.92"/>
+    </svg>
+  `,
+
+  forecastNormal: `
+    <svg class="app-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+      <path d="M6 22a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h8a2.4 2.4 0 0 1 1.704.706l3.588 3.588A2.4 2.4 0 0 1 20 8v12a2 2 0 0 1-2 2z"/>
+      <path d="M14 2v5a1 1 0 0 0 1 1h5"/>
+      <path d="m16 13-3.5 3.5-2-2L8 17"/>
+    </svg>
+  `,
+
+  forecastGrowing: `
+    <svg class="app-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+      <path d="M12 16v5"/>
+      <path d="M16 14.639V21"/>
+      <path d="M20 10.656V21"/>
+      <path d="m22 3-8.646 8.646a.5.5 0 0 1-.708 0L9.354 8.354a.5.5 0 0 0-.707 0L2 15"/>
+      <path d="M4 18.463V21"/>
+      <path d="M8 14.656V21"/>
+    </svg>
+  `,
+
+  forecastOverBudget: `
+    <svg class="app-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+      <path d="m21.73 18-8-14a2 2 0 0 0-3.48 0l-8 14A2 2 0 0 0 4 21h16a2 2 0 0 0 1.73-3"/>
+      <path d="M12 9v4"/>
+      <path d="M12 17h.01"/>
+    </svg>
+  `,
+
+  forecastDanger: `
+    <svg class="app-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+      <circle cx="12" cy="13" r="8"/>
+      <path d="M5 3 2 6"/>
+      <path d="m22 6-3-3"/>
+      <path d="M6.38 18.7 4 21"/>
+      <path d="M17.64 18.67 20 21"/>
+      <path d="M9 13h6"/>
+    </svg>
+  `,
+
+  recurringEmpty: `
+    <svg class="app-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+      <circle cx="12" cy="12" r="10"/>
+      <path d="M22 2 2 22"/>
+    </svg>
+  `,
+
+  recurringNormal: `
+    <svg class="app-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+      <path d="M11 10v4h4"/>
+      <path d="m11 14 1.535-1.605a5 5 0 0 1 8 1.5"/>
+      <path d="M16 2v4"/>
+      <path d="m21 18-1.535 1.605a5 5 0 0 1-8-1.5"/>
+      <path d="M21 22v-4h-4"/>
+      <path d="M21 8.5V6a2 2 0 0 0-2-2H5a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h4.3"/>
+      <path d="M3 10h4"/>
+      <path d="M8 2v4"/>
+    </svg>
+  `,
+
+  recurringMany: `
+    <svg class="app-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+      <path d="M21 12a9 9 0 0 0-9-9 9.75 9.75 0 0 0-6.74 2.74L3 8"/>
+      <path d="M3 3v5h5"/>
+      <path d="M3 12a9 9 0 0 0 9 9 9.75 9.75 0 0 0 6.74-2.74L21 16"/>
+      <path d="M16 16h5v5"/>
+      <circle cx="12" cy="12" r="1"/>
+    </svg>
+  `,
+
+  recurringHigh: `
+    <svg class="app-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+      <path d="M3.85 8.62a4 4 0 0 1 4.78-4.77 4 4 0 0 1 6.74 0 4 4 0 0 1 4.78 4.78 4 4 0 0 1 0 6.74 4 4 0 0 1-4.77 4.78 4 4 0 0 1-6.75 0 4 4 0 0 1-4.78-4.77 4 4 0 0 1 0-6.76Z"/>
+      <path d="M7 12h5"/>
+      <path d="M15 9.4a4 4 1 0 0 0 0 5.2"/>
+    </svg>
+  `,
+
+  adviceDefault: `
+    <svg class="app-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+      <path d="M15 14c.2-1 .7-1.7 1.5-2.5 1-.9 1.5-2.2 1.5-3.5A6 6 0 0 0 6 8c0 1 .2 2.2 1.5 3.5.7.7 1.3 1.5 1.5 2.5"/>
+      <path d="M9 18h6"/>
+      <path d="M10 22h4"/>
+    </svg>
+  `,
+
+  adviceGood: `
+    <svg class="app-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+      <path d="M3.85 8.62a4 4 0 0 1 4.78-4.77 4 4 0 0 1 6.74 0 4 4 0 0 1 4.78 4.78 4 4 0 0 1 0 6.74 4 4 0 0 1-4.77 4.78 4 4 0 0 1-6.75 0 4 4 0 0 1-4.78-4.77 4 4 0 0 1 0-6.76Z"/>
+      <path d="m9 12 2 2 4-4"/>
+    </svg>
+  `,
+
+  adviceWarning: `
+    <svg class="app-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+      <path d="M2.992 16.342a2 2 0 0 1 .094 1.167l-1.065 3.29a1 1 0 0 0 1.236 1.168l3.413-.998a2 2 0 0 1 1.099.092 10 10 0 1 0-4.777-4.719"/>
+      <path d="M12 8v4"/>
+      <path d="M12 16h.01"/>
+    </svg>
+  `,
+
+  adviceSaving: `
+    <svg class="app-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+      <path d="M11 17h3v2a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1v-3a3.16 3.16 0 0 0 2-2h1a1 1 0 0 0 1-1v-2a1 1 0 0 0-1-1h-1a5 5 0 0 0-2-4V3a4 4 0 0 0-3.2 1.6l-.3.4H11a6 6 0 0 0-6 6v1a5 5 0 0 0 2 4v3a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1z"/>
+      <path d="M16 10h.01"/>
+      <path d="M2 8v1a2 2 0 0 0 2 2h1"/>
+    </svg>
+  `,
+
+  habitBrain: `
+    <svg class="app-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+      <path d="M12 18V5"/>
+      <path d="M15 13a4.17 4.17 0 0 1-3-4 4.17 4.17 0 0 1-3 4"/>
+      <path d="M17.598 6.5A3 3 0 1 0 12 5a3 3 0 1 0-5.598 1.5"/>
+      <path d="M17.997 5.125a4 4 0 0 1 2.526 5.77"/>
+      <path d="M18 18a4 4 0 0 0 2-7.464"/>
+      <path d="M19.967 17.483A4 4 0 1 1 12 18a4 4 0 1 1-7.967-.517"/>
+      <path d="M6 18a4 4 0 0 1-2-7.464"/>
+      <path d="M6.003 5.125a4 4 0 0 0-2.526 5.77"/>
+    </svg>
+  `,
+
+  habitActivity: `
+    <svg class="app-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+      <path d="M12 2v10"/>
+      <path d="M18.4 6.6a9 9 0 1 1-12.77.04"/>
+    </svg>
+  `,
+
+  habitNight: `
+    <svg class="app-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+      <path d="M13 16a3 3 0 0 1 0 6H7a5 5 0 1 1 4.9-6z"/>
+      <path d="M18.376 14.512a6 6 0 0 0 3.461-4.127c.148-.625-.659-.97-1.248-.714a4 4 0 0 1-5.259-5.26c.255-.589-.09-1.395-.716-1.248a6 6 0 0 0-4.594 5.36"/>
+    </svg>
+  `,
+
+  habitFriday: `
+    <svg class="app-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+      <path d="M12.127 22H5a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2v5.125"/>
+      <path d="M14.62 18.8A2.25 2.25 0 1 1 18 15.836a2.25 2.25 0 1 1 3.38 2.966l-2.626 2.856a.998.998 0 0 1-1.507 0z"/>
+      <path d="M16 2v4"/>
+      <path d="M3 10h18"/>
+      <path d="M8 2v4"/>
+    </svg>
+  `,
+
+  habitUp: `
+    <svg class="app-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+      <path d="M16 7h6v6"/>
+      <path d="m22 7-8.5 8.5-5-5L2 17"/>
+    </svg>
+  `,
+
+  habitDown: `
+    <svg class="app-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+      <path d="M16 17h6v-6"/>
+      <path d="m22 17-8.5-8.5-5 5L2 7"/>
+    </svg>
+  `,
 };
